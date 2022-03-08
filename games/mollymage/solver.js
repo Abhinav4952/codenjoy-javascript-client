21951bbd0061f7ce48310430867b33ffd147e731
Abhinav Veeramosu <abhinav.veeramosu@ehealthCorp.net>
2022-03-08 15:26:30 +0530

    Create a different version of solver

--- games/mollymage/solver.js
+++ games/mollymage/solver.js
@@ -20,22 +20,82 @@
  * #L%
  */
 
+let lastPos = [], posCount = 0, timer = 0;
+function hasObstacleHorizontal(hero, direction, isBarrierAt) {
+    if(!isBarrierAt(hero.x-1, hero.y)){
+        return direction.LEFT;
+    }
+    if(!isBarrierAt(hero.x+1, hero.y)){
+        return direction.RIGHT;
+    }
+    return hasObstacleVertical(hero, direction, isBarrierAt);
+}
 
+function hasObstacleVertical(hero, direction, isBarrierAt, isFutureBarrierAt) {
+    if(!isBarrierAt(hero.x, hero.y-1)){
+        return direction.DOWN;
+    }
+    if(!isBarrierAt(hero.x, hero.y+1)){
+        return direction.UP;
+    }
+    return hasObstacleHorizontal(hero, direction, isBarrierAt, isFutureBarrierAt);
+}
+
+var extendBoard = (function(board, Point){
+    board.isBarrierAt = function(x, y) {
+        if (new Point(x, y).isOutOf(board.size)) {
+            return true;
+        }
+
+        return board.contains(board.getBarriers(), new Point(x, y));
+    };
+    board.getMyBarriers = function() {
+        var all = board.getGhosts();
+        all = all.concat(board.getWalls());
+        all = all.concat(board.getTreasureBoxes());
+        all = all.concat(board.getOtherHeroes());
+        return board.removeDuplicates(all);
+    };
+    board.isMyBarrierAt = function(x, y) {
+        if (new Point(x, y).isOutOf(board.size)) {
+            return true;
+        }
+
+        return board.contains(board.getMyBarriers(), new Point(x, y));
+    };
+    return board;
+})
 var MollymageSolver = module.exports = {
 
-    get: function (board) {
-        /**
-         * @return next hero action
-         */
+get: function (board) {
+    /**
+     * @return next hero action
+     */
+   
+    var Games = require('./../../engine/games.js');
+    var Point = require('./../../engine/point.js');
+    var Direction = Games.require('./direction.js');
+    var Element = Games.require('./elements.js');
+    var Stuff = require('./../../engine/stuff.js');
+    board = extendBoard(board, Point);
 
-        var Games = require('./../../engine/games.js');
-        var Point = require('./../../engine/point.js');
-        var Direction = Games.require('./direction.js');
-        var Element = Games.require('./elements.js');
-        var Stuff = require('./../../engine/stuff.js');
-
-        // TODO your code here
-
-        return Direction.ACT;
+    const myHero = board.getHero();
+    if(timer >= 5) {
+        timer = 0;
     }
+    timer += 1;
+    if(lastPos[0] == "ACT" && posCount <= 2 && timer <= 3){
+        posCount += 1;
+        if(posCount === 1){
+            return hasObstacleHorizontal(myHero, Direction, board.isMyBarrierAt);
+        }
+        lastPos = [];
+        posCount = 0;
+        return hasObstacleVertical(myHero, Direction, board.isMyBarrierAt);
+    }
+    let nextDir = Direction.ACT;
+    lastPos = ['ACT'];
+    posCount = 0;
+    return nextDir;
+}
 };
